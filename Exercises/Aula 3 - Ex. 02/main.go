0d649be6223de141d9fc968ec419b82a11bd2847
Jovi <101208535+Joviviz@users.noreply.github.com>
2024-04-03 12:08:59 -0300

    Update main.go

--- Exercises/Aula 3 - Ex. 02/main.go
+++ Exercises/Aula 3 - Ex. 02/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Numeros primos de 1 a 19: \n")
+	fmt.Print("Numeros impares de 1 a 19: \n")
 	for i := 1; i < 20; i++ {
 		if i%2 != 0 {
 			fmt.Printf("%d \n", i)
