4986db1c77afccc1718ae01c6e55c6dd3b5bf919
Jovi <101208535+Joviviz@users.noreply.github.com>
2024-04-04 18:15:27 -0300

    Create main.go

---
+++ Exercises/Aula 4 - Ex. 05/main.go
@@ -0,0 +1,28 @@
+package main
+import "fmt"
+
+func main() {
+
+    fmt.Println("Lista com os numeros : 1, 2, 3, 4, -1, -2, -3, -4")
+    numeros := []float64{1, 2, 3, 4, -1, -2, -3, -4}
+    
+    encontrarNumero(numeros)
+}
+
+func encontrarNumero(numeros []float64) float64{
+    var x float64
+    
+    fmt.Println("Escolha um numero na lista: ")
+    fmt.Scanln(&x)
+    
+    for i := 0; i < len(numeros); i++ {
+        if x == numeros[i]{
+            fmt.Printf("O numero %.2f esta na lista", numeros[i])
+            return x
+            break
+        } 
+    }
+    
+    fmt.Printf("O numero %.2f nao esta na lista", x)
+    return 0
+}
