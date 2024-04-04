12d2ea878453afcf55e2eeb4036994e6515f760d
Jovi <101208535+Joviviz@users.noreply.github.com>
2024-04-04 16:25:16 -0300

    Create main.go

---
+++ Exercises/Aula 4 - Ex. 03/main.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+func main() {
+    fmt.Println("Lista com os numeros : 1, 2, 3, 4")
+    numeros := []float64{1, 2, 3, 4}
+    
+    media := calcularMedia(numeros)
+    fmt.Println("A media = ", media)
+    
+}
+
+func calcularMedia(numeros []float64) float64{
+    total := 0.0
+    
+    for i := 0; i < len(numeros); i++ {
+        total += numeros[i]
+    }
+    return(total/float64(len(numeros)))
+}
